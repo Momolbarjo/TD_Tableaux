a25db34870851fdab6b50f1208390731bb8b6fb0
Momolbarjo <119139743+Momolbarjo@users.noreply.github.com>
2022-12-11 19:10:41 +0100

    Update exo2td8(pas fini).c

--- exo2td8(pas fini).c
+++ exo2td8(pas fini).c
@@ -21,16 +21,87 @@ srand(time(NULL));
     }
 }
 
+float averageArray(int tab[],int size){
+
+float somme=0;
+for(int i=0;i<size;i++){
+
+  float valeurTmp=tab[i];
+  somme=somme+valeurTmp;
+
+}
+float moyenne=somme/size;
+
+printf("La moyenne est de %f\n",moyenne);
+
+return(moyenne);
 
 
 
 
 
 
+}
+
+int maxArray(int tab[], int size){
+int tmp_max = tab[0];
+for(int i=0;i<size;i++){
+    if(tmp_max<tab[i]){
+        tmp_max = tab[i];
+    }
+    else {tmp_max = tmp_max;}
+}
+printf("Le max est de %d\n",tmp_max);
+return(tmp_max);
+}
+
+int minArray(int tab[],int size){
+int tmp_min = tab[0];
+for(int i=0;i<size;i++){
+ if (tmp_min> tab[i]){
+        tmp_min=tab[i];
+
+   }
+    else {tmp_min = tmp_min;}
+ }
+
+   printf("Le min est de %d",tmp_min);
+   return(tmp_min);
+}
+
+
+int searchElmt(int tab[], int size, int elmt){
+
+for(int i=0;i<size;i++){
+ if (tab[i]==elmt){
+        printf("\nCela correspond a tab[%d]",i);
+         return(tab[i]);
+
+}
+   /*else {
+        printf("\n-1");
+        return (-1);}
+*/
+}
+}
+
+
+
+void main () {
+
+int tab[25];
+
+ fillArray(tab,SIZE);
+ printArray(tab,SIZE);
+ averageArray(tab,SIZE);
+ maxArray(tab,SIZE);
+ minArray(tab,SIZE);
+searchElmt(tab,SIZE,15);
 
 
 
 
+}
 
 
 
